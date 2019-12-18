636fcb13292ce22aef84b7adbaa8a64b05196299
ayurkin <alx.yurkin@gmail.com>
2019-12-18 17:58:06 +0300

    add write elements (in process)

--- ElementsWriter.py
+++ ElementsWriter.py
@@ -1,6 +1,49 @@
+class GroupsWriterToAutocadPage(object):
+    def __init__(self, file_name, page_lines_count, modelspace, template_name):
+        self.file_name = file_name
+        self.page_lines_count = page_lines_count
+        self.modelspace = modelspace
+        self.template_name = template_name
+        self.__file = self.create_file(self.template_name, self.modelspace)
+        self.__left_groups = None
+
+    def create_file(self, template_name, modelspace):
+        return modelspace.Documents.Add(template_name)
+
+    def save_file(self):
+        self.__file.SaveAs(self.file_name)
+        file_path = self.__file.FullName
+        self.__file.Close()
+        return file_path
+
+    def write_groups(self, groups):
+        pass
+
+
+class GroupsWriterToAutocadFiles(object):
+    ELEMENTS_LIST_PAGE_NAME = "elements_list"
+    FIRST_PAGE_TEMPLATE = "a4-1-PE_v2.dwt"
+    OTHER_PAGE_TEMPLATE = "a4-2-PE_v2.dwt"
+    FORMAT = "dwg"
+    FIRST_PAGE_LINES_COUNT = 10
+    OTHER_PAGE_LINES_COUNT = 10
+
+    def __init__(self):
+        self.elements_list_files = []
+
+    def write_groups(self, groups, modelspace):
+        first_page_file_name = "{name}_1.{ext}".format(name=self.ELEMENTS_LIST_PAGE_NAME, ext=self.FORMAT)
+        first_page_writer = GroupsWriterToAutocadPage(first_page_file_name, self.OTHER_PAGE_LINES_COUNT, modelspace,
+                                                      self.FIRST_PAGE_TEMPLATE)
+        first_page_writer.write_groups(groups)
+
+
 class ElementsListWriter(object):
-    # def __init__(self, groups_writer):
-        # self.groups_witer = groups_writer()
+    GROUPS_WRITER = GroupsWriterToAutocadFiles
+
+    def __init__(self):
+        self.groups_writer = self.GROUPS_WRITER()
+        self.elements_list_files = []
 
     def get_sorted_by_tag(self, elements):
         s = sorted(elements, key=lambda elem: elem['tag'])
@@ -23,13 +66,12 @@ class ElementsListWriter(object):
                 groups.append(ElementsGroup([element]))
         return groups
 
-    def write_elements(self, elements):
-        elements_sorted = self.get_sorted_by_tag(elements)
-        groups = self.get_groups(elements_sorted)
+    def write_elements(self, elements, modelspace):
+        elements_sorted = self.get_sorted_by_tag(elements)  # Sort elements by tag
+        groups = self.get_groups(elements_sorted) # Create groups by the same catalog number
+        self.elements_list_files = self.groups_writer(groups, modelspace)
 
 
-class GroupsWriterToFiles(object):
-    pass
 
 
 class ElementsGroup(object):
